0a69efaab32898ec4970f7837ac157c5e6cd8857
ahmedhaniyemen@gmail.com <ahmedhaniyemen@gmail.com>
2022-11-17 18:29:46 +0100

    Add Ldap-Verbindung Klasse

---
+++ LdapManager/LdapAuthorization.cs
@@ -0,0 +1,45 @@
+﻿using System.DirectoryServices.Protocols;
+using System.Net;
+
+namespace BookingPlatform.LoginManager
+{
+
+   /* //Use this Class with this Code
+      LdapAuthorization ldap = new LdapAuthorization("Login", "login-dc-01.login.htw-berlin.de");
+
+      //Test mit deinem MatrNr und Passwort
+      return ldap.ValidateByBind("MatrNr", "Password"); */
+
+    public class LdapAuthorization
+    {
+        NetworkCredential? credential { get; set; }
+        LdapConnection? connection { get; set; }
+
+        private readonly LdapDirectoryIdentifier identifier;
+        private readonly string domain;
+
+        public LdapAuthorization(string domainPara, string url)
+        {
+            identifier = new LdapDirectoryIdentifier(url);
+            this.domain = domainPara;
+        }
+
+        public bool ValidateByBind(string matNr, string password)
+        {
+            bool response = true;
+            credential = new NetworkCredential(matNr, password, this.domain);
+            this.connection = new LdapConnection(identifier, credential);
+            try
+            {
+                connection.Bind();
+            }
+            catch (Exception)
+            {
+                response = false;
+            }
+
+            connection.Dispose();
+            return response;
+        }
+    }
+}
