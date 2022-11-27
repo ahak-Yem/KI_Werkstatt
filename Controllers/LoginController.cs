49a5c053f725dd896d897690dc4a6e05e8930dae
Ahmed Hani Abdulatif Kutbi <Ahmed.Kutbi@Student.HTW-Berlin.de>
2022-11-28 00:40:46 +0100

    Add Login-Form to Program

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using BookingPlatform.LoginManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingPlatform.Controllers
+{
+    public class LoginController : Controller
+    {
+        public bool isaccountvalid = false;
+
+
+        public IActionResult Index(string MatrNr, string Passwort)
+        {
+            LdapAuthorization ldap = new LdapAuthorization("Login", "login-dc-01.login.htw-berlin.de");
+            isaccountvalid = ldap.ValidateByBind($"{MatrNr}", $"{Passwort}");
+
+            if (isaccountvalid == false)
+            {
+                ModelState.AddModelError("Fehler", "Prüfen Sie nochmal Ihre Login-Daten und schalten Sie Ihre HTW-Vpn an!.");
+            }
+            if (isaccountvalid == true && ModelState.IsValid)
+            {
+                isaccountvalid = true;
+                return RedirectToAction("Index", "Ressources");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
